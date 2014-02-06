85b61494849f46ce0d3ccf5417d8304cf3a87a2d
Sumer Mixon <gitSumer@ximm.us>
2014-02-06 08:53:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Working Theme
+Based on the [Base Theme](https://github.com/ximmus/base-theme)
+
+## WP Files:
+- style.css
+- index.php
+- sass: style.scss
+
+## Production Tool Files:
+- .gitignore
+- README.md (this file)
+- .sublime-project
+- config.rb
---
+++ config.rb
@@ -0,0 +1,19 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "sass"
+
+# Sytax:
+preferred_syntax = :scss
+
+# Use comment to pick environment
+
+# PRODUCTION:
+#environment  = :production
+#output_style = :compressed
+
+# DEVELOPMENT:
+environment  = :development
+output_style = :expanded
+
+# more info on this file here: http://compass-style.org/help/tutorials/configuration-reference/
